a6d0a06e2c4f57a81fd84551a892af6eebe0c49b
mario <mariokavula@gmail.com>
2020-07-28 11:26:15 +0200

    theme init, layout and frontpage basic done

--- storage/framework/classes.php
+++ storage/framework/classes.php
@@ -3,4 +3,9 @@
   'backend\\Controllers\\index' => 'modules\\backend\\controllers\\index.php',
   'backend\\Controllers\\auth' => 'modules\\backend\\controllers\\auth.php',
   'cms\\Controllers\\index' => 'modules\\cms\\controllers\\index.php',
+  'backend\\Controllers\\media' => 'modules\\backend\\controllers\\media.php',
+  'system\\Controllers\\settings' => 'modules\\system\\controllers\\settings.php',
+  'cms\\Controllers\\themes' => 'modules\\cms\\controllers\\themes.php',
+  'system\\Controllers\\updates' => 'modules\\system\\controllers\\updates.php',
+  'rainlab\\builder\\Plugin' => 'plugins\\rainlab\\builder\\Plugin.php',
 );
